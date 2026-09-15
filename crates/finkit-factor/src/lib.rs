56101813e9d0520c92b67f65b79d2643f01a0be9
coeasy <mdonetwo@yeah.net>
2026-09-15 17:37:37 +0800

    feat: expose factor result and prepare v2 factor api

--- crates/finkit-factor/src/lib.rs
+++ crates/finkit-factor/src/lib.rs
@@ -4,9 +4,11 @@ use finkit_series::QuantSeries;
 
 mod ema;
 mod sma;
+mod result;
 
 pub use ema::Ema;
 pub use sma::Sma;
+pub use result::FactorResult;
 
 pub trait Factor {
     fn name(&self) -> &str;
