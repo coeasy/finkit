ebe47456a53f702839f489dd71a425c3eb490324
coeasy <mdonetwo@yeah.net>
2026-09-15 18:02:53 +0800

    feat: export macd factor

--- crates/finkit-factor/src/lib.rs
+++ crates/finkit-factor/src/lib.rs
@@ -4,13 +4,15 @@ use finkit_series::QuantSeries;
 
 mod ema;
 mod sma;
-mod result;
 mod rsi;
+mod macd;
+mod result;
 
 pub use ema::Ema;
 pub use sma::Sma;
-pub use result::FactorResult;
 pub use rsi::Rsi;
+pub use macd::Macd;
+pub use result::FactorResult;
 
 pub trait Factor {
     fn name(&self) -> &str;
